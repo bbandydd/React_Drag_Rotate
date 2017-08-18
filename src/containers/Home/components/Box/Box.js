b779697736f39cd05f8c02f0036d934170107f66
Andy Tsai <bbandydd@gmail.com>
2017-08-18 11:24:25 +0800

    add rotate function

--- src/containers/Home/components/Box/Box.js
+++ src/containers/Home/components/Box/Box.js
@@ -17,6 +17,12 @@ export default class Box extends Component {
 
     this.props.onSetting(index, 'x', x);
     this.props.onSetting(index, 'y', y);
+
+    // 旋轉公式 以中心點為圓心，逆時針旋轉
+    // x' = cos * x - sin * y
+    // 中心點x + Math.round(Math.cos(90 / 180 * Math.PI) * 100) / 100 * 50 - Math.round(Math.sin(90 / 180 * Math.PI) * 100) / 100 * 50
+    // y' = sin * x + cos * y
+    // 中心點y + Math.round(Math.sin(90 / 180 * Math.PI) * 100) / 100 * 50 + Math.round(Math.cos(90 / 180 * Math.PI) * 100) / 100 * 50
   }
 
   render() {
@@ -26,7 +32,7 @@ export default class Box extends Component {
     } = this.props;
 
     const style = {
-      transform: `translate(${x}px, ${y}px) rotate(${rotate}deg)`,
+      transform: `translate(${x}px, ${y}px) rotate(${rotate * -1}deg)`,
       width: `${width / scale}px`,
       height: `${height / scale}px`,
       position: 'absolute',
